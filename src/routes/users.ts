bdd279624982052c9fc278e866e114a28eb49114
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-01 09:21:45 -0300

    feat(routes/users): route to create a new user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,23 @@
-import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    console.log('hello')
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const requestBodySchema = z.object({
+      username: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { email, password, username } = requestBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      email,
+      password,
+      username,
+    })
+
+    reply.status(201).send()
   })
 }
