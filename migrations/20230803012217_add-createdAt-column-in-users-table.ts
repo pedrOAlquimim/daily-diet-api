ac19e2755cf340567e12bc7f48524b8714db2d5d
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-04 21:01:58 -0300

    chore: create migration to create a new meals table and add created_at to users

---
+++ migrations/20230803012217_add-createdAt-column-in-users-table.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dateTime('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropColumn('created_at')
+  })
+}
---
+++ migrations/20230803013030_create-meals-table.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid()).notNullable()
+    table.text('title').index().notNullable()
+    table.text('description')
+    table.date('day').notNullable()
+    table.time('hour')
+    table.boolean('is_on_the_diet').defaultTo(false).notNullable()
+
+    table.uuid('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
