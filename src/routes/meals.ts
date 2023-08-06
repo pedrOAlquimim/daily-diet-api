4d0327c878bb33716b3b954e68043bf446723de2
Pedro Alquimim <pedrohalquimim@gmail.com>
2023-08-05 21:00:18 -0300

    feat: create route to update a specific meal

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -38,4 +38,46 @@ export async function mealsRoute(app: FastifyInstance) {
       reply.status(201).send()
     },
   )
+
+  app.put(
+    '/:id',
+    { preHandler: [userIdCookieExists] },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const requestParamsSchema = z.object({
+        id: z.string(),
+      })
+
+      const { id } = requestParamsSchema.parse(request.params)
+
+      const requestBodySchema = z.object({
+        title: z.string(),
+        description: z.string(),
+        day: z.string(),
+        hour: z.string(),
+        isOnTheDiet: z.boolean(),
+      })
+
+      const { title, description, day, hour, isOnTheDiet } =
+        requestBodySchema.parse(request.body)
+
+      const ptToUsDate = convertPtBrToEnUsDate(day)
+
+      const cookieUserId = request.cookies.daily_diet_userId
+
+      const _formattedDate = new Date(ptToUsDate).toISOString().split('T')[0]
+      const formattedDate = new Date(_formattedDate + 'T' + hour)
+
+      await knex('meals')
+        .where('id', id)
+        .andWhere('user_id', cookieUserId)
+        .update({
+          title,
+          description,
+          is_on_the_diet: isOnTheDiet,
+          time: formattedDate,
+        })
+
+      reply.status(201).send()
+    },
+  )
 }
